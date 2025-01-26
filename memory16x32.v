fc1a5413e846b9aa26ba27cdbd882dce622ac126
Ziad Abdelfattah <1901658@eng.asu.edu.eg>
2025-01-26 22:20:15 +0200

    Initial Design

--- memory16x32.v
+++ memory16x32.v
@@ -1,24 +1,37 @@
-module memort16x32;
+module memort16x32 #(
+    parameter DATA_WIDTH = 32,
+    parameter ADDR_WIDTH = 4,
+    parameter MEMO_DEPTH = (1 << ADDR_WIDTH)
+)(
+    input   [DATA_WIDTH-1:0] Data_in;
+    input   [ADDR_WIDTH-1:0] Address;
+    input   EN, CLK, RST;
+    output  reg [DATA_WIDTH-1:0] Data_out;
+    output  reg Valid_out;
+);
 
-input [31:0] Data_in;
-input [3:0] Address;
-input EN, CLK, RST;
-output reg [31:0] Data_out;
-output reg [3:0] Valid_out;
+reg [DATA_WIDTH-1:0] memory [0:MEMO_DEPTH-1];
 
-always@(posedge clk or posedge rst)
+always@(posedge CLK or posedge RST)
 begin
 
     if(rst) begin
         Data_out  <= 'b0;
         Valid_out <= 'b0;
     end else begin
-
-    
+        //Enable high = write
+        if(EN) begin
+            memory[Address] <= Data_in;
+            Valid_out <= 'b0;
+        end
+        
+        //Enable low = read
+        else begin
+            Data_out <= memory[Address];
+            Valid_out <= 'b1;
+        end
     end
 
 end
-
-
 
 endmodule
